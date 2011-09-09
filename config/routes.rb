5b1d9b0a8f7337bd50338dbb232c4a70dd58f087
lavanya <lavanya@indigenius.com>
2011-09-09 15:11:57 +0530

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookmarks::Application.routes.draw do
+  get "home/index"
+
   #hey lavanya initiate the project...
   #ghouse will continue with the database..
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Bookmarks::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "home#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
